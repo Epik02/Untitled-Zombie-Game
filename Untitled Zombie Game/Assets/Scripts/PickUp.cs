81b22efe56ae2b66865df384bc259a766de82efa
QueenLinlay <leejusti217@gmail.com>
2022-09-23 13:45:14 -0400

    Update

---
+++ Untitled Zombie Game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public bool equipped;
+public static bool slotFull;
+
+public class PickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
