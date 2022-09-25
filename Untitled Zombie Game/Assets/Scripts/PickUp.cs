54f8286eedd1df226a5fa0c8f5b3a7523635b5ef
QueenLinlay <91083842+QueenLinlay@users.noreply.github.com>
2022-09-25 18:32:05 -0400

    Update PickUp (not finish)

--- Untitled Zombie Game/Assets/Scripts/PickUp.cs
+++ Untitled Zombie Game/Assets/Scripts/PickUp.cs
@@ -3,22 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
-public bool equipped;
-public static bool slotFull;
+//public bool equipped;
+//public static bool slotFull;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //public Projectilegun gunScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
     {
-        
+        // Setup
+        if(!equipped)
+        {
+            //gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+        if (equipped)
+        {
+            //gunScript.enable = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Check if player is in range and "E" is pressed
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.manitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUps();
+
+        //Drop if equipped and "Q" is pressed
+        if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
     }
 
+    private void PickUps()
+    {
+        equipped = true;
+        slotFull = true;
+
+        //Make weapon a child of the character/camera and move it to default position
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        //Enable script
+        //gunScript.enabled = true;
+
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        //Set parent to null
+        transform.SetParent(null);
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        //Gun carries momentum of player
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        //AddForce
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+        //Add random rotation
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random)*10);
+
+
+        //Enable script
+        //gunScript.enabled = false;
+    }
 }
+
