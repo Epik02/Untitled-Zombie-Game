1b09d759acb6c97076cd481ab59981afc704f48c
QueenLinlay <91083842+QueenLinlay@users.noreply.github.com>
2023-01-25 15:51:37 -0500

    Update New added Cost and Models (temp) for Mystery Box and Purchase Gun - in progress

--- Untitled Zombie Game/Assets/PurchaseGun.cs
+++ Untitled Zombie Game/Assets/PurchaseGun.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PurchaseGun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject WeaponSold;
+
+    bool intrigger = false;
+
+    public int PurchaseCost = 0;
+    public TMP_Text Cost;
+    public GameObject CostText;
+    public string WeaponName;
+
+
+    PickUp grabWeaponName;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.name == "Capsule")
+        {
+            intrigger = true;
+            Debug.Log("Encounter Weapon Purchase");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.gameObject.name == "Capsule")
+        {
+            intrigger = false;
+            Debug.Log("Weapon Purchase False");
+        }
+    }
+
+    private void Update()
+    {
+        if (intrigger == true)
+        {
+            CostText.SetActive(true);
+            Cost.text = PurchaseCost.ToString() + " points to purchase " + WeaponName;
+            if (Input.GetKeyDown(KeyCode.E) == true && ScoreManager.instance.GetScore() >= PurchaseCost)
+            {
+                Debug.Log("Purchase Weapon Working");
+                Instantiate(WeaponSold, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            }
+        }
+        else
+        {
+            CostText.SetActive(false);
+        }
     }
 }
