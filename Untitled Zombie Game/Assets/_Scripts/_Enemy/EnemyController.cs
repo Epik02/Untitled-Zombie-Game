eb2f8c8d6eba5253cc529a7f27e607c685022e6a
QueenLinlay <91083842+QueenLinlay@users.noreply.github.com>
2023-04-15 05:18:20 -0400

    Adding Zombie Sound + Fixing + Adding Volume Difference if Zombie is closer to player or not
    Audio Added with reference: Zombie Attack Sound
    Improve EnemyController Script: Added difference of distance to adjust zombie volume
    Fix Blue Gun

--- Untitled Zombie Game/Assets/_Scripts/_Enemy/EnemyController.cs
+++ Untitled Zombie Game/Assets/_Scripts/_Enemy/EnemyController.cs
@@ -10,6 +10,15 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private InformationValues informationValues;
 
+    //public AudioClip[] audioOptions;
+
+    public AudioSource zombieAudio;
+
+    public GameObject Player;
+
+    public float maxDistance = 0.5f; // The distance at which the zombie sound is at maximum volume
+    public float minDistance = 20f; // The distance at which the zombie sound is at minimum volume
+
     // public GameObject EnemyHealth;
     public int damages;
 
@@ -21,6 +30,9 @@ public class EnemyController : MonoBehaviour
         ScoreManager.instance.AddEnemy();
         damages = informationValues.damage._EnemyDamage;
         transform.GetComponent<Rigidbody>().WakeUp();
+        zombieAudio = GetComponent<AudioSource>();
+        //PlayerRandomAudio();
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnDisable()
@@ -28,6 +40,13 @@ public class EnemyController : MonoBehaviour
         //ScoreManager.instance.DecreaseEnemy();
         transform.GetComponent<Rigidbody>().Sleep();
     }
+
+    //private void PlayerRandomAudio()
+   // {
+       // int randomIndex = Random.Range(0, audioOptions.Length);
+        //zombieAudio.clip = audioOptions[randomIndex];
+       // zombieAudio.Play();
+   // }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -49,5 +68,14 @@ public class EnemyController : MonoBehaviour
     public void SetDamage(int value)
     {
         informationValues.damage._EnemyDamage = value;
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        //float maxDistance = 1f; // The distance at which the zombie sound is at maximum volume
+        //float minDistance = 8f; // The distance at which the zombie sound is at minimum volume
+        float volume = Mathf.InverseLerp(minDistance, maxDistance, distance);
+        zombieAudio.volume = volume;
     }
 }
